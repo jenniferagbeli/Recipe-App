b84cefa63f1f45286f2a75087c29442a37bb7788
jenniferagbeli <agbelij12@gmail.com>
2024-02-27 00:10:41 +1200

    fetching recipes from API

--- src/pages/recipes/index.jsx
+++ src/pages/recipes/index.jsx
@@ -1,28 +1,42 @@
 import { Grid, Card, CardMedia, Typography, CardContent, Container, TextField, } from "@mui/material";
+import RecipeItem from "../../components/recipe-item";
+import { useEffect, useState } from "react";
 
 export default function Recipes() {
-    return (
-        <Container sx={{my: '2rem'}} maxWidth="sm">
-         <TextField 
-         fullWidth 
-         id="outlined-basic" 
-         label="Enter a keyboard to search recipes and hit Enter"
-         variant="outlined" />
+    const [recipes, setRecipes] = useState([]);
 
-            <Grid sx={{ mt:'1rem'}} container spacing={3}>
-                <Grid item xs={4}>
-                <Card>
-                    <CardMedia 
-                    sx={{height: 140}}
-                    image="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505"/>
-                </Card>
-                <CardContent>
-                    <Typography variant="h5">Potash</Typography>
-                </CardContent>
+    const searchRecipes = () => {
+        // prepare url
+        const url = new URL('https://api.spoonacular.com/recipes/complexSearch');
+        url.searchParams.append('apiKey', '682ecbd217144c468f8557a129534144');
+        // fetch recipes
+        fetch(url)
+            // .then((response) => {return response.json()})
+            .then((response) => response.json())
+            .then((data) => {
+                // update the recipes state
+               setRecipes(data.results);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    useEffect(searchRecipes, []);
+
+    return (
+        <Container sx={{ my: '2rem' }}>
+            <TextField
+                fullWidth
+                id="outlined-basic"
+                label="Enter a keyboard to search recipes and hit Enter"
+                variant="outlined" />
+
+            <Grid sx={{ mt: '1rem' }} container spacing={3}>
+                {recipes.map((recipe) => <RecipeItem key={recipe.id} title={recipe.title} image={recipe.image} />)}
             </Grid>
-         </Grid>
         </Container>
-    );
+    )
 }
 // or
 
